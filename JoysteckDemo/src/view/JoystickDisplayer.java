eed9269805d67ec4d1f0211a028206e1d26a8142
Admin <Admin@ASUS>
2020-01-12 23:45:59 +0200

    joystick moves

--- JoysteckDemo/src/view/JoystickDisplayer.java
+++ JoysteckDemo/src/view/JoystickDisplayer.java
@@ -8,15 +8,33 @@ import javafx.beans.property.StringProperty;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
 
 public class JoystickDisplayer extends Canvas {
 
 	
+
 	private StringProperty joystickFileName;
+	double x=1;
+	double y=1;
+	double xPosD=0;
+	double yPosD=0;
+	double xPos;
+	double yPos;
+	double radius;
+	String str="helloWorld";
+	
 	
 	
 	public JoystickDisplayer() {
 		joystickFileName=new SimpleStringProperty();
+
+		xPos=getWidth()/4;
+		yPos=getHeight()/4;
+	
 	}
 	
 	public String getJoystickFileName() {
@@ -34,12 +52,14 @@ public class JoystickDisplayer extends Canvas {
 
 
 	public void redraw() {
-		
-		double W=getWidth();
-		double H=getHeight();
 		GraphicsContext gc = getGraphicsContext2D();
 		Image joystick=null;
+		double W=getWidth();
+		double H=getHeight();
 		
+		this.xPosD=W/4;
+		this.yPosD=H/4;
+		this.radius=H/2-H/3;
 		
 		try {
 			joystick=new Image(new FileInputStream(joystickFileName.get()));
@@ -47,11 +67,58 @@ public class JoystickDisplayer extends Canvas {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		gc.clearRect(0, 0, W, H);
+		
 		if(joystick==null)
 			System.out.println("problem");
-		gc.drawImage(joystick,W/2, H/2, W, H);
 		
+		//need only for first time to put joystick in the center
+		if(x==1) {
+			x++;
+			xPos=xPosD;
+			yPos=yPosD;
+		}
+		gc.drawImage(joystick,xPos,yPos, W/2, H/2);
+	}
+	public void moveJoysticTo(double x, double y) {
 		
+		/*System.out.println("///////");
+		System.out.println((Math.pow((x-xPosD),2)));
+		System.out.println(Math.pow((y-yPosD),2));
+		System.out.println(Math.pow(radius,2));*/
+		if((Math.pow((x-xPosD),2)+Math.pow((y-yPosD),2))<Math.pow(radius,2)) {
+			setxPos(x);
+			setyPos(y);
+			
+		}
+		else {
+			double dist=Math.sqrt((Math.pow((x-xPosD),2)+Math.pow((y-yPosD),2)));
+			double xA=(x-xPos)/dist;
+			double yA=(y-yPos)/dist;
+			setxPos(xA*radius+xPosD);
+			setyPos(yA*radius+yPosD);
+			
+		}
+	}
+	
+	
+	public void setJoystickToDefultPosition() {
+		
+		this.xPos=this.xPosD;
+		this.yPos=this.yPosD;
+		redraw();
+	}
+
+
+	
+
+	public void setxPos(double xPos) {
+		this.xPos = xPos;
+	}
+
+	public void setyPos(double yPos) {
+		this.yPos = yPos;
 	}
 	
 	
--- JoysteckDemo/src/view/WindowController.java
+++ JoysteckDemo/src/view/WindowController.java
@@ -1,11 +1,22 @@
 package view;
 
+import java.awt.MouseInfo;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Slider;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
+import javafx.stage.Stage;
 import view_model.ViewModel;
 
 public class WindowController implements Initializable{
@@ -30,6 +41,30 @@ public class WindowController implements Initializable{
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		joystickDisplayer.redraw();
+		
+		//joystickDisplayer.addEventFilter(MouseEvent.MOUSE_DRAGGED, (e)->joystickDisplayer.move());
+		joystickDisplayer.setOnMouseDragged(new EventHandler <MouseEvent>() {
+	            public void handle(MouseEvent event) {
+	                /* drag was detected, start drag-and-drop gesture*/
+	                System.out.println("onDragDetected");
+	                
+	                
+	                	System.out.println(event.getSceneX());
+	                
+		                joystickDisplayer.moveJoysticTo(event.getSceneX()-100,event.getSceneY()-150);
+		             
+		                joystickDisplayer.redraw();
+	                
+	            }
+	        });
+
+		joystickDisplayer.setOnMouseReleased(new EventHandler <MouseEvent>() {
+            public void handle(MouseEvent event) {
+                /* drag was detected, start drag-and-drop gesture*/
+                System.out.println("onDragReleesd");
+                joystickDisplayer.setJoystickToDefultPosition();
+            }
+        });
 	}
 	
 }
